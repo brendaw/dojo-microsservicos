95b2e37e61ef3cf7124d03ff4e35bc80e78f3294
William Brendaw <williambrendaw@protonmail.com>
2020-07-07 19:09:18 -0300

    Add base client-service

---
+++ client-base/src/main/java/org/dojo/client/ClientServiceApplication.java
@@ -0,0 +1,13 @@
+package org.dojo.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientServiceApplication.class, args);
+	}
+
+}
---
+++ client-base/src/test/java/org/dojo/client/ClientServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.dojo.client;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
