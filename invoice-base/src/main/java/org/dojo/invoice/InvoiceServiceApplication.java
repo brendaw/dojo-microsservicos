daf730bb196b08a12bc7c3ea64b71415ae891788
William Brendaw <williambrendaw@protonmail.com>
2020-07-08 19:20:49 -0300

    Add base invoice-service

---
+++ invoice-base/src/main/java/org/dojo/invoice/InvoiceServiceApplication.java
@@ -0,0 +1,13 @@
+package org.dojo.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceServiceApplication.class, args);
+	}
+
+}
